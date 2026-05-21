266fb0e1b07025e38d8dc977821c1324065745a3
momothiam-dev <mamadou.thiam16@unchk.edu.sn>
2026-05-21 16:43:19 +0000

    Add sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://afrique-tech-hub.vercel.app',
+  generateRobotsTxt: true,
+  // optional: you can customize the robots.txt or exclude routes here
+};
