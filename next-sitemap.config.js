e79a44b9a04b70cb137b349b24dac6b1c11bd140
momothiam-dev <mamadou.thiam16@unchk.edu.sn>
2026-05-21 16:58:46 +0000

    Fix sitemap output directory to public

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   siteUrl: 'https://afrique-tech-hub.vercel.app',
   generateRobotsTxt: true,
+  outDir: 'public',
   // optional: you can customize the robots.txt or exclude routes here
 };
